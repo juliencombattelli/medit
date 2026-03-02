834d68dd897c39cdb243e75ba5ab88ff2fbf1a32
Julien Combattelli <julien.combattelli@gmail.com>
2026-03-02 17:04:37 +0100

    Add text input handling

--- src/main.c
+++ src/main.c
@@ -103,42 +103,62 @@ int main(int argc, char** argv)
     bool running = true;
     SDL_ShowWindow(window);
 
-    SDL_Event e;
+    SDL_StartTextInput(window);
+
+#define TEXT_CAPACITY (size_t)(1024 * 1024)
+    char text[TEXT_CAPACITY] = { 0 };
+    size_t text_size = 0;
+
+    SDL_Event event;
     while (running) {
-        while (SDL_PollEvent(&e) != 0) {
+        while (SDL_PollEvent(&event) != 0) {
             int win_width = 0;
             int win_height = 0;
             SDL_GetWindowSize(window, &win_width, &win_height);
-            switch (e.type) {
+            switch (event.type) {
                 case SDL_EVENT_QUIT:
                     running = false;
                     break;
                 case SDL_EVENT_KEY_DOWN: {
-                    if (e.key.scancode == SDL_SCANCODE_ESCAPE) {
+                    if (event.key.scancode == SDL_SCANCODE_ESCAPE) {
                         running = false;
                     }
-                    if (e.key.scancode == SDL_SCANCODE_UP) {
+                    if (event.key.scancode == SDL_SCANCODE_UP) {
                         if (meditor.cursor_y != 0) {
                             meditor.cursor_y -= 1;
                         }
                     }
-                    if (e.key.scancode == SDL_SCANCODE_DOWN) {
+                    if (event.key.scancode == SDL_SCANCODE_DOWN) {
                         if ((meditor.cursor_y + 1) * meditor.char_height
                             < win_height) {
                             meditor.cursor_y += 1;
                         }
                     }
-                    if (e.key.scancode == SDL_SCANCODE_LEFT) {
+                    if (event.key.scancode == SDL_SCANCODE_LEFT) {
                         if (meditor.cursor_x != 0) {
                             meditor.cursor_x -= 1;
                         }
                     }
-                    if (e.key.scancode == SDL_SCANCODE_RIGHT) {
+                    if (event.key.scancode == SDL_SCANCODE_RIGHT) {
                         if ((meditor.cursor_x + 1) * meditor.char_width
                             < win_width) {
                             meditor.cursor_x += 1;
                         }
                     }
+                } break;
+                case SDL_EVENT_TEXT_INPUT: {
+                    size_t input_size = strlen(event.text.text);
+                    const size_t free_space = TEXT_CAPACITY - text_size;
+                    if (text_size > free_space) {
+                        text_size = free_space;
+                    }
+                    memcpy(text + text_size, event.text.text, input_size);
+                    text_size += input_size;
+                    int text_width = 0;
+                    // TODO TTF_GetStringSize only on the input string and
+                    // increment the cursor
+                    TTF_GetStringSize(font, text, text_size, &text_width, NULL);
+                    meditor.cursor_x = text_width / meditor.char_width;
                 } break;
             }
         }
@@ -154,10 +174,16 @@ int main(int argc, char** argv)
         render_text0(&meditor, "TrueType Font Rendering", 10, 4, white);
         render_text0(&meditor, "With SDL_ttf", 10, 6, cyan);
         render_text0(&meditor, "Press ESC to exit", 10, 10, lime);
+
+        if (text_size != 0) {
+            render_text0(&meditor, text, 0, 0, white);
+        }
         render_cursor(&meditor, white);
 
         SDL_RenderPresent(renderer);
     }
+
+    SDL_StopTextInput(window);
 
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
