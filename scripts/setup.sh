e23a18d711615d1355bc7d01743e51f9d8d85a2b
Julien Combattelli <julien.combattelli@gmail.com>
2026-03-03 09:33:15 +0100

    Use user-installed packages first, then fetchcontent if not found

---
+++ scripts/setup.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+set -eu
+
+usage() {
+    echo "Install Medit dependencies at a desired place."
+    echo
+    echo "Usage:"
+    echo "  setup.sh SRC_DIR [INSTALL_DIR]"
+    echo
+    echo "Arguments:"
+    echo "  SRC_DIR         The path where to clone and build the projects."
+    echo "  INSTALL_DIR     The path where to install the projects. Optional."
+    echo "                    If not specified, a default value is used by CMake."
+}
+
+clone_or_checkout() (
+    url="$1"
+    branch_or_tag="$2"
+    src_dir="$3"
+    if [ ! -d "$src_dir" ]; then
+        git clone "$url" "$src_dir" -b "$branch_or_tag" --depth=1 --recursive
+    else
+        # TODO consider not doing a checkout to allow changes in dependencies
+        git -C "$src_dir" checkout "$branch_or_tag"
+    fi
+)
+
+SRC_DIR="$1"
+INSTALL_DIR="${2:-}"
+
+if [ -z "$SRC_DIR" ]; then
+    echo "Missing argument"
+    usage
+    exit 1
+fi
+
+if [ ! -d "$SRC_DIR" ]; then
+    mkdir -p "$SRC_DIR"
+fi
+
+# Rely on "$@" for more robust word splitting
+set --
+if [ -n "$INSTALL_DIR" ]; then
+    set -- "-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR"
+    if [ ! -d "$INSTALL_DIR" ]; then
+        mkdir -p "$INSTALL_DIR"
+    fi
+fi
+
+# Download dependencies
+clone_or_checkout https://github.com/libsdl-org/SDL release-3.4.2 "$SRC_DIR/SDL"
+clone_or_checkout https://github.com/libsdl-org/SDL_ttf release-3.2.2 "$SRC_DIR/SDL_ttf"
+
+# Build and install SDL (static and shared)
+cmake -S "$SRC_DIR/SDL" -B "$SRC_DIR/SDL/build" \
+    -DCMAKE_BUILD_TYPE=Release "$@" \
+    -DSDL_SHARED=ON -DSDL_STATIC=ON -DSDL_INSTALL_DOCS=ON
+cmake --build "$SRC_DIR/SDL/build" -j
+cmake --install "$SRC_DIR/SDL/build"
+
+# Build and install SDL_ttf (shared)
+cmake -S "$SRC_DIR/SDL_ttf" -B "$SRC_DIR/SDL_ttf/build" \
+    -DCMAKE_BUILD_TYPE=Release "$@" \
+    -DBUILD_SHARED_LIBS=ON -DSDLTTF_INSTALL_MAN=ON -DSDLTTF_VENDORED=ON
+cmake --build "$SRC_DIR/SDL_ttf/build" -j
+cmake --install "$SRC_DIR/SDL_ttf/build"
+
+# Build and install SDL_ttf (static)
+cmake -S "$SRC_DIR/SDL_ttf" -B "$SRC_DIR/SDL_ttf/build" \
+    -DCMAKE_BUILD_TYPE=Release "$@" \
+    -DBUILD_SHARED_LIBS=OFF -DSDLTTF_INSTALL_MAN=ON -DSDLTTF_VENDORED=ON
+cmake --build "$SRC_DIR/SDL_ttf/build" -j
+cmake --install "$SRC_DIR/SDL_ttf/build"
