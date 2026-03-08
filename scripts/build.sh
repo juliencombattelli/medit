ca3151a6e5a438c70e047d652b0001f0b837d79b
Julien Combattelli <julien.combattelli@gmail.com>
2026-03-08 22:57:43 +0100

    Use consola from Windows if available

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,19 @@
 
 echo "Downloading fonts..."
 mkdir -p asset/font
-wget https://github.com/hfg-gmuend/openmoji/raw/16.0.0/font/OpenMoji-color-colr0_svg/OpenMoji-color-colr0_svg.ttf \
-    --output-document=asset/font/OpenMoji-color-colr0_svg.ttf
-wget https://github.com/google/fonts/raw/refs/heads/main/ofl/inconsolata/static/Inconsolata-Regular.ttf \
-    --output-document=asset/font/Inconsolata-Regular.ttf
+if [ ! -f asset/font/OpenMoji-color-colr0_svg.ttf ]; then
+    wget https://github.com/hfg-gmuend/openmoji/raw/16.0.0/font/OpenMoji-color-colr0_svg/OpenMoji-color-colr0_svg.ttf \
+        --output-document=asset/font/OpenMoji-color-colr0_svg.ttf --timeout=10
+fi
+if [ ! -f asset/font/Inconsolata-Regular.ttf ]; then
+    wget https://github.com/google/fonts/raw/refs/heads/main/ofl/inconsolata/static/Inconsolata-Regular.ttf \
+        --output-document=asset/font/Inconsolata-Regular.ttf --timeout=10
+fi
+if [ ! -f asset/font/consola.ttf ]; then
+    if [ -f /mnt/c/Windows/Fonts/consola.ttf ]; then
+        cp /mnt/c/Windows/Fonts/consola.ttf asset/font/consola.ttf
+    fi
+fi
 
 cmake -S . -B build \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo -DFETCHCONTENT_QUIET=NO \
