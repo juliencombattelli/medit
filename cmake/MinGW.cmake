161a3428cd526ade47c6578b82183779c38c3b46
Julien Combattelli <julien.combattelli@gmail.com>
2026-03-30 13:43:47 +0200

    Fix MinGW builds

--- cmake/MinGW.cmake
+++ cmake/MinGW.cmake
@@ -1,16 +1,21 @@
+# toolchain-mingw64.cmake
 set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# which compilers to use for C and C++
-set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+# specify the cross compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
-# where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32ucrt)
+# where is the target environment
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 
-# adjust the default behavior of the FIND_XXX() commands:
-# search programs in the host environment
+# search for programs in the build host directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
-# search headers and libraries in the target environment
+# for libraries and headers in the target directories
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# CMake determines how to examine dependencies based on the *host* system, leading to
+# a `file unknown error` unless the target platform is explicitly specified.
+set(CMAKE_GET_RUNTIME_DEPENDENCIES_PLATFORM "windows+pe")
