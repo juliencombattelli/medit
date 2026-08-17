bdf5d5e5747164fbabe047b99a307f60bf34e8f2
Julien Combattelli <julien.combattelli@gmail.com>
2026-08-17 19:35:24 +0200

    Start a Ui module

---
+++ playground/ui-clay/theme.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../../src/core/color.h"
+
+typedef struct {
+    Color editor_fg;
+    Color editor_bg;
+    Color line_number;
+    Color line_number_current;
+    Color sidebar_bg;
+    Color cursor;
+    Color menu_bar_bg;
+    Color status_bar_bg;
+    Color tab_bar_bg;
+    Color tab_bar_bg_hovered;
+    Color tab_bar_bg_displayed;
+    Color bottom_panel_bg;
+    Color panel_border;
+    Color scrollbar_thumb;
+    Color scrollbar_thumb_scroll_area_hovered;
+    Color scrollbar_thumb_hovered;
+} ColorTheme;
+
+typedef struct {
+    ColorTheme colors;
+    uint8_t dragged_tab_transparency;
+    uint32_t title_bar_height;
+    uint32_t title_bar_button_width;
+    uint32_t window_resize_border;
+    uint32_t scrollbar_size;
+    uint32_t scrollbar_corner_radius;
+} UiTheme;
---
+++ playground/ui-clay/ui.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "clay_utils.h"
+#include "theme.h"
+
+static inline Clay_Color to_clay_color(Color color) {
+    return (Clay_Color) { .r = color.r, .g = color.g, .b = color.b, .a = color.a };
+}
+
+typedef struct {
+    const char* container_id;
+    ScrollContainerConfig config;
+} ScrollContainerCustom;
+
+typedef struct {
+    UiTheme theme;
+    MouseState mouse_state;
+    DragState drag_state;
+    const ScrollContainerCustom* scroll_containers;
+    const size_t scroll_container_count;
+} Ui;
+
+Ui medit_ui_init(void);
+
+bool medit_ui_is_any_element_dragged(const Ui* ui);
+
+void medit_ui_update_scroll_containers(Ui* ui);
+
+// Lay out a scrollbar with predefined settings for the opened element
+void medit_ui_layout_scrollbar(const Ui* ui);
